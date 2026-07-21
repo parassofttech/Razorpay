ee23d0f57967d9c783517a2653dff50143088d1d
Paras Sahu <paras2115s@gmail.com>
2026-07-21 23:20:59 +0530

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Home from "./pages/Home"
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div>hello</div>
+   <div>
+    <Home/>
+   </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import HeroSection from '../home/HeroSection'
+import AcceptPayments from '../home/AcceptPayments'
+import Functionality from '../home/Functionality'
+import PaymentGateway from '../home/PaymentGateway'
+import MakePayouts from '../home/MakePayouts'
+import BusinessBanking from '../home/BusinessBanking'
+import Ecommmerce from '../home/Ecommmerce'
+import Feature from '../home/Feature'
+import Footer from '../home/Footer'
 
 const Home = () => {
   return (
     <div>
+      <Navbar/>
+      <HeroSection/>
+      <PaymentGateway/>
       
+      <Functionality/>
+      <AcceptPayments/>
+      <MakePayouts/>
+      <BusinessBanking/>
+      <Ecommmerce/>
+      <Feature/>
+      <Footer/>
     </div>
   )
 }
